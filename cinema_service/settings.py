6a2ea5ba230f1fcc910859acbadb77196e7496be
Leo <leonsiypopov@gmail.com>
2025-04-13 20:17:16 +0300

    Fixed flake8

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -20,9 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-6vubhk2$++agnctay_4pxy_8cq)mosmn(*-#2b^v4cgsh-^!i3"
-)
+SECRET_KEY = ("django-insecure-6vubhk2$"
+              "++agnctay_4pxy_8cq)mosmn(*-#2b^v4cgsh-^!i3")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,12 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     "rest_framework",
     "rest_framework.authtoken",
-
     "debug_toolbar",
-
     "cinema",
     "user",
 ]
@@ -119,12 +115,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication'
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": (
-        'cinema.permissions.IsAdminOrIfAuthenticatedReadOnly',
-    )
+        "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly",
+    ),
 }
 
 AUTH_USER_MODEL = "user.User"
