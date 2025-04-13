cd28e01415bc8fd78948052bc0f054034afdb41f
Leo <leonsiypopov@gmail.com>
2025-04-13 07:59:31 +0300

    not done'

--- user/urls.py
+++ user/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from user import views
 
 urlpatterns = [
-    path("register/", views.UserCreateView.as_view(), name="register"),
+    path("register/", views.UserCreateView.as_view(), name="create"),
     path("login/", views.UserLoginView.as_view(), name="login"),
-    path("me/", views.ManageUserView.as_view(), name="me"),
+    path("me/", views.ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
